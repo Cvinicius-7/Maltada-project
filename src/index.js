52357b4635863e6056634c0cf54f94ad934306f2
Carlos Vinícius <p.carlosvini@gmail.com>
2025-09-01 20:48:00 -0300

    Ajustes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
