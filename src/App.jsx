66903c7a94b745476f328d2a147799b7c27496d6
Carlos Vinícius <p.carlosvini@gmail.com>
2025-09-01 22:09:57 -0300

    Ajustes de Estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { supabase } from './services/SupabaseClient';
 
 const App = () => {
+    console.log(supabase)
+
     return (
         <div>
             <h1>Bem-vindo ao Projeto Bloco Atualizado e com deploy automatizado!</h1>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'; // 1. Importe o 'React'
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-// 2. Use a função 'createRoot' que você importou diretamente
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
